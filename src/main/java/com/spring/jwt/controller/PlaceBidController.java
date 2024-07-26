61c74604c89ad0479648d2b928868ca1cbac03c0
Ashutosh7796 <ashutoshshedgeas87@gmail.com>
2024-07-26 20:55:55 +0530

    ADDED: GetAllFInalBidsByDealerID

--- src/main/java/com/spring/jwt/controller/PlaceBidController.java
+++ src/main/java/com/spring/jwt/controller/PlaceBidController.java
@@ -1,13 +1,13 @@
 package com.spring.jwt.controller;
 
 import com.spring.jwt.Interfaces.PlacedBidService;
+import com.spring.jwt.dto.*;
 import com.spring.jwt.dto.BeedingDtos.PlacedBidDTO;
-import com.spring.jwt.dto.ResponseAllPlacedBidDTO;
-import com.spring.jwt.dto.ResponseDto;
-import com.spring.jwt.dto.ResponseSinglePlacedBid;
 import com.spring.jwt.entity.FinalBid;
 import com.spring.jwt.exception.*;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +20,9 @@ import java.util.List;
 public class PlaceBidController {
 
     private final PlacedBidService placedBidService;
+
+    private static final Logger logger = LoggerFactory.getLogger(PlaceBidController.class);
+
 
     @PostMapping("/placeBid")
     private ResponseEntity<?> placeBid(@RequestBody PlacedBidDTO placedBidDTO, @RequestParam Integer bidCarId) {
@@ -66,7 +69,7 @@ public class PlaceBidController {
     public ResponseEntity<ResponseAllPlacedBidDTO> getTopThreeBids(@PathVariable Integer bidCarId) {
         try {
             List<PlacedBidDTO> topThreeBids = placedBidService.getTopThree(bidCarId);
-            return ResponseEntity.ok(new ResponseAllPlacedBidDTO("Top three bids for car ID " + bidCarId + " retrieved successfully", topThreeBids, null));
+            return ResponseEntity.ok(new ResponseAllPlacedBidDTO("Top three bids for car ID "  + " retrieved successfully", null, null));
         } catch (BidNotFoundExceptions e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseAllPlacedBidDTO(e.getMessage(), null, null));
         } catch (Exception e) {
@@ -79,6 +82,24 @@ public class PlaceBidController {
         List<FinalBid> finalBids = placedBidService.getAllFinalBids();
         return ResponseEntity.ok(finalBids);
     }
+
+    @GetMapping("/getAllDealerFinalBids")
+    public ResponseEntity<ResponseFinalBidsAll> getAllDealer(@RequestParam Integer buyerDealerId) {
+        try {
+            logger.debug("Received request to fetch all dealer final bids for buyerDealerId: {}", buyerDealerId);
+            List<FinalBidDto> getbids = placedBidService.getDealerAllBids(buyerDealerId);
+            ResponseFinalBidsAll dtoo = new ResponseFinalBidsAll("Success");
+            dtoo.setFinalBids(getbids);
+
+            return ResponseEntity.status(HttpStatus.OK).body(dtoo);
+        } catch (RuntimeException e) {
+            logger.error("Error fetching dealer final bids for buyerDealerId: " + buyerDealerId, e);
+            ResponseFinalBidsAll profile = new ResponseFinalBidsAll("Unsuccessful");
+            profile.setException(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(profile);
+        }
+    }
+
 
     @GetMapping("/car/{bidCarId}")
     public ResponseEntity<ResponseAllPlacedBidDTO> getPlacedBidsByCarId(@PathVariable Integer bidCarId) {
