1ea5a2f1135c18e0907ef8a24f05c03801701568
Ashutosh7796 <ashutoshshedgeas87@gmail.com>
2024-08-08 13:34:51 +0530

    FIXED: TimeZone Issue

--- src/main/java/com/spring/jwt/controller/StartBidingController.java
+++ src/main/java/com/spring/jwt/controller/StartBidingController.java
@@ -26,6 +26,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.*;
@@ -68,6 +70,13 @@ public class StartBidingController {
         }
         try {
             LocalDateTime endTime = biddingTimerRequest.getEndTime();
+            // Log the end time
+            System.out.println("Received end time: " + endTime);
+
+            // Optionally convert to UTC or any specific time zone for storage
+            ZonedDateTime endTimeZoned = endTime.atZone(ZoneId.of("Asia/Kolkata"));
+            System.out.println("End time in Asia/Kolkata: " + endTimeZoned);
+
             BiddingTimerRequestDTO savedRequest = biddingTimerService.startTimer(biddingTimerRequest);
             biddingTimerRequests.put(savedRequest.getBiddingTimerId(), savedRequest);
             scheduleTask(savedRequest.getBiddingTimerId(), endTime);
