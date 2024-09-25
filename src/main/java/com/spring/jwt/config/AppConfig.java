047abf28af23e541c32fce56ed05c2c6a4596071
tkraj888 <tusharck88@gmail.com>
2024-09-25 15:01:23 +0530

    color added

--- src/main/java/com/spring/jwt/config/AppConfig.java
+++ src/main/java/com/spring/jwt/config/AppConfig.java
@@ -130,6 +130,7 @@ public class AppConfig {
                 .requestMatchers("/userFormController/**").permitAll()
                 .requestMatchers("/saveCar/**").permitAll()
                 .requestMatchers("/ProfilePhoto/**").permitAll()
+                .requestMatchers("/colors/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .authenticationManager(manager)
